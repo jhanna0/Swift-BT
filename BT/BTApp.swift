c6c7b901fd200de4eed0cb15e7fa1baf1a32df59
Jad <JadHanna603@gmail.com>
2022-07-17 21:31:22 -0700

    Initial Commit

---
+++ BT/BTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTApp.swift
+//  BT
+//
+//  Created by Jad Hanna on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct BTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BT
+//
+//  Created by Jad Hanna on 7/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
